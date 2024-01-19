c0ca8dca2768e4909ac14da49a1aa30867e9383a
pk <piyushkulkarni422@gmail.com>
2024-01-19 14:34:50 +0530

    removed exports

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
 
-import Cpage from "./_components/Canvas";
-import App from "./_components/eg";
-import Rect from "./_components/rectcanvas";
-import Rough from "./_components/roughcanvas";
 import Rough2 from "./_components/roughcanvas2";
 
 
